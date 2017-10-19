b5ab0080c1ecf6b201a1a595b6c87ad96751b3cb
brian-cooney <bcooney@gmail.com>
2017-10-19 14:45:10 +0100

    Video description added

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,11 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
-
-
-
-$(document).ready(function(){
-  $('.your-class').slick({
-    dots: true
-  });
-});
